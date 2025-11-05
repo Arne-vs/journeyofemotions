fdf79f8a24d6a9d5722de9264a6e5903b478eb2d
unknown <arnescha2003@gmail.com>
2025-11-05 16:11:28 +0100

    fix build pls i would be gratefull

--- next.config.js
+++ next.config.js
@@ -1,12 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // IMPORTANT: do NOT set output: 'export'
+  reactStrictMode: true,
   async headers() {
     return [
       {
         source: "/audio/tracks/:path*",
-        headers: [{ key: "Cache-Control", value: "public, max-age=31536000, immutable" }]
-      }
+        headers: [{ key: "Cache-Control", value: "public, max-age=31536000, immutable" }],
+      },
     ];
-  }
+  },
 };
+
 export default nextConfig;
